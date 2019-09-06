da6dcbce2e469f676eaf972a421ff5696fc82ffb
“Constantine” <constantine.bon@gmail.com>
2019-09-07 01:59:37 +0300

    The script removes subdomains properly

--- script.py
+++ script.py
@@ -4,59 +4,41 @@ import subprocess
 import sys,json
 
 
-
-e = ""
-
-def func():
-
-	p = subprocess.Popen(['uapi', '--user=c0nst', 'DomainInfo', 'list_domains', '--output=json'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	o,e = p.communicate()
+p = subprocess.Popen(['uapi', '--user=c0nst', 'DomainInfo', 'list_domains', '--output=json'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+o,e = p.communicate()
 	
 
 
-	if e:
-        	print("The following errors occurred: \n")
-        	print(e)
-        	ans =  raw_input("Should I proceed? ")
-        	if ans == "Y":
-			print('proceeding')
-			return o
-		else:
-			return 
-
-#if e:
-#	print("The following errors occurred: \n")
-#	print(e)
-#	ans =  raw_input("Should I proceed? ")
-#	if ans == "Y":
+if e:
+	print("The following errors occurred: \n")
+	print(e)
+	ans =  raw_input("Should I proceed? ")
+	if ans == "Y":
+		print('proceeding')
 
 
-
-
-func()
-a = json.loads(o)
-sdoms = a["result"]["data"]["sub_domains"]
-addons = a["result"]["data"]["addon_domains"]
-print(sdoms)
-print(addons)
+		a = json.loads(o)
+		sdoms = a["result"]["data"]["sub_domains"]
+		addons = a["result"]["data"]["addon_domains"]
+		print(sdoms)
+		print(addons)
 		
 
-for i in addons:
-	print("%s" % (i))
+		for i in addons:
+			print("%s" % (i))
 
-for i in sdoms:
-	print("%s" % (i))
+		for i in sdoms:
+			print("%s" % (i))
 
 
-bul = raw_input("Are you sure you want to remove all these pretties? ")
+		bul = raw_input("Are you sure you want to remove all these pretties? ")
 
-if bul == "Y":
-	for i in sdoms:
-		print("Removed")
+		if bul == "Y":
+			for i in sdoms:
+				#b = subprocess.Popen(['cpapi2', '--user=c0nst', 'SubDomain', 'delsubdomain, 'domain=i'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+				b = subprocess.call(['cpapi2', '--user=c0nst', 'SubDomain', 'delsubdomain', 'domain=%s' % (i)])
+				print("Removed")
+		else:
+			print('Break!') 
 else:
-	print('Break!') 
-#	else:
-#		print('Break!')
-
-#else: 
-#	print('Break!')
+	print('Break!')
